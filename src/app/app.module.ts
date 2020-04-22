8e9905f8ca30855b8498dbaaaef72ad3247ed598
Daan Vissers <daanvissers81@gmail.com>
2020-04-22 12:40:26 +0200

    Angular project with routing, Material components, Angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        AppRoutingModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,17 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyAxsplwY46IZUOdHJ8ieoP2yfNW1uX9qRc",
+  authDomain: "taskboard-2889d.firebaseapp.com",
+  databaseURL: "https://taskboard-2889d.firebaseio.com",
+  projectId: "taskboard-2889d",
+  storageBucket: "taskboard-2889d.appspot.com",
+  messagingSenderId: "707425009012",
+  appId: "1:707425009012:web:dcb79be56b4158f334c021",
+  measurementId: "G-TXT24C4J07"
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
