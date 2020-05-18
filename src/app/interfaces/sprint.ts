7bbde1f8e309e92ec004e48dfe762d1553de7535
Daan Vissers <daanvissers81@gmail.com>
2020-05-18 21:03:19 +0200

    Create interfaces for Sprint, User-Story

---
+++ src/app/interfaces/sprint.ts
@@ -0,0 +1,6 @@
+export interface Sprint {
+    title: string,
+    description: string,
+    startDate: Date,
+    endDate: Date,
+}
---
+++ src/app/interfaces/user-story.ts
@@ -0,0 +1,7 @@
+export interface UserStory {
+    title: string,
+    description: string,
+    status: string,
+    storyPoints: number,
+    owner: string
+}
