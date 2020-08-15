4186a236b574cc78a9d99a3c165ade5df9926698
Daan Vissers <daanvissers81@gmail.com>
2020-08-15 22:30:14 +0200

    [Tests] A few tests...

--- src/app/components/projects/project-list/project-list.component.spec.ts
+++ src/app/components/projects/project-list/project-list.component.spec.ts
@@ -10,6 +10,9 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { of } from 'rxjs';
 import { ProjectsService } from 'src/app/services/projects.service';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('ProjectsComponent', () => {
   let component: ProjectListComponent;
   let fixture: ComponentFixture<ProjectListComponent>;
@@ -20,14 +23,15 @@ describe('ProjectsComponent', () => {
 
     serviceStub = {
       getAll: () => of([{
-        description: 'sadfsda',
+        id: 'xxx1',
+        description: 'This is a description.',
         isArchived: false,
         members: [
           {role: 'Owner', uid: 'asfddasf'},
           {role: 'Owner', uid: 'asfddasf'},
         ],
-        name: 'asdfadsf',
-        owner: 'asfasdf',
+        name: 'Example project',
+        owner: 'Administrator',
         status: 'In Progress'
       }])
     };
@@ -35,11 +39,13 @@ describe('ProjectsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ ProjectListComponent ],
       imports: [  RouterModule.forRoot([]),
+                  BrowserAnimationsModule,
                   MatSnackBarModule,
                   MatDialogModule,
                   AngularFireModule.initializeApp(firebaseConfig),
                   AngularFireAuthModule,
-                  AngularFirestoreModule, 
+                  AngularFirestoreModule,
+                  MatExpansionModule
                 ],
       providers: [
         { provide: ProjectsService, useValue: serviceStub }
@@ -49,7 +55,6 @@ describe('ProjectsComponent', () => {
   }));
 
   beforeEach(() => {
-
     fixture = TestBed.createComponent(ProjectListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -57,6 +62,28 @@ describe('ProjectsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  })
+  });
+
+  it(`should display the project's title`, async () => {
+    const headerTitle = document.querySelector('mat-expansion-panel-header mat-panel-title')
+                                .textContent;
+    expect(headerTitle).toContain('Example project');
+  });
+
+  it(`should display the project's status`, async () => {
+    const headerStatus = document.querySelector('mat-expansion-panel-header mat-panel-description')
+                                 .textContent;
+    expect(headerStatus).toContain('In Progress');
+  });
+
+  // it(`should display the project's description`, async () => {
+  //   const description = document.querySelector('.description').textContent;
+  //   expect(description).toContain('This is a description.');
+  // });
+
+  // it(`should have two members`, async () => {
+  //   const chips = document.getElementsByClassName('member').length;
+  //   expect(chips).toBe(2);
+  // })
 
 });
